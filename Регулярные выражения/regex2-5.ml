8f4d576057227a576202a61e5d12885232681b8d
ivgl <49949308+ivgl@users.noreply.github.com>
2021-08-29 20:56:01 +0300

    Update regex2-5.ml

--- Регулярные выражения/regex2-5.ml
+++ Регулярные выражения/regex2-5.ml
@@ -1,3 +1,5 @@
+(*2-5 Задание*)
+
 let re = Str.regexp "\\(\\([1-9][0-9]*\\)\\|0\\)\\(\\.\\(\\([0-9]*\\([1-9]\\)\\)\\|0\\)\\)?$" ;;  
 let n = read_line();;
 if Str.string_match re n 0 then print_string "Matched" else print_string "Failed";;
