f302d8f8eea43e7ff4f3831717d6434e5d000786
ivgl <49949308+ivgl@users.noreply.github.com>
2021-08-29 20:54:57 +0300

    Create regex2-5.ml

---
+++ Регулярные выражения/regex2-5.ml
@@ -0,0 +1,3 @@
+let re = Str.regexp "\\(\\([1-9][0-9]*\\)\\|0\\)\\(\\.\\(\\([0-9]*\\([1-9]\\)\\)\\|0\\)\\)?$" ;;  
+let n = read_line();;
+if Str.string_match re n 0 then print_string "Matched" else print_string "Failed";;
